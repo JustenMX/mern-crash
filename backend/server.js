367e76b24b726805867c01d1328d8c4fa7b96990
Justen <me@justenmanni.dev>
2024-10-24 15:06:35 +0800

    Add mongoose schema for Product model

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 //controller
-app.get("/", (req, res) => {
-  res.send("Server is ready Sir Justen");
+app.get("/products", (req, res) => {
+  //res.send("Server is ready Sir Justen");
 });
 
-app.listen(6000, () => {
-  console.log("Server started at http://localhost:6000");
+app.listen(4000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:4000");
 });
